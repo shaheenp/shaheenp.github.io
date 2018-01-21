7bb0a9bbf9d800578769d98adc4c22b943469608
shaheenp <shaheenspage@gmail.com>
2018-01-20 18:26:01 -0800

    add header, footer & night mode toggle

---
+++ js/index.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    function onload () {
+        const sunsetElement = document.querySelector('img#sunset');
+        const sunriseElement = document.querySelector('img#sunrise');
+
+        sunsetElement.addEventListener('click', function () {
+            document.body.className = 'night';
+            sunriseElement.style.display = '';
+            sunsetElement.style.display = 'none';
+        });
+
+        sunriseElement.addEventListener('click', function () {
+            document.body.className = 'day';
+            sunsetElement.style.display = '';
+            sunriseElement.style.display = 'none';
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', onload);
+})();
