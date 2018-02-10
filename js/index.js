733e610ec4eb352036db55c47b70297ac49d070e
shaheenp <shaheenspage@gmail.com>
2018-02-10 11:38:33 -0800

    redesign & added social links

--- js/index.js
+++ js/index.js
@@ -1,21 +1,44 @@
 (function () {
     'use strict';
 
+    const socialLinks = {
+        linkedin: 'https://www.linkedin.com/in/shaheenpage',
+        github: 'https://www.github.com/shaheenp'
+    };
+
+    function linkifySocialImages () {
+        const socialNames = Object.getOwnPropertyNames(socialLinks);
+
+        for (const name of socialNames) {
+            document.querySelector(`img[data-social="${name}"]`).addEventListener('click', () => {
+                window.location.href = socialLinks[name];
+            });
+        }
+    }
+
     function onload () {
-        const containerElement = document.querySelector('div#container');
         const sunsetElement = document.querySelector('img#sunset');
         const sunriseElement = document.querySelector('img#sunrise');
+        const socialElements = Array.from(document.querySelectorAll('img.social'));
 
-        sunsetElement.addEventListener('click', function () {
-            containerElement.className = 'night';
-            sunriseElement.style.display = '';
+        linkifySocialImages();
+
+        sunsetElement.addEventListener('click', () => {
+            document.body.classList.add('night');
             sunsetElement.style.display = 'none';
+            sunriseElement.style.display = '';
+            socialElements.forEach(img => {
+                img.style.display = img.classList.contains('social-night') ? '' : 'none';
+            });
         });
 
-        sunriseElement.addEventListener('click', function () {
-            containerElement.className = 'day';
+        sunriseElement.addEventListener('click', () => {
+            document.body.classList.remove('night');
             sunsetElement.style.display = '';
             sunriseElement.style.display = 'none';
+            socialElements.forEach(img => {
+                img.style.display = img.classList.contains('social-night') ? 'none' : '';
+            });
         });
     }
 
