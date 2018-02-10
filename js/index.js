7d8dbe226e934cbff3f2f669ecad63c2e38abf6d
shaheenp <shaheenspage@gmail.com>
2018-02-10 14:55:51 -0800

    fix social links

--- js/index.js
+++ js/index.js
@@ -1,27 +1,10 @@
 (function () {
     'use strict';
 
-    const socialLinks = {
-        linkedin: 'https://www.linkedin.com/in/shaheenpage',
-        github: 'https://www.github.com/shaheenp'
-    };
-
-    function linkifySocialImages () {
-        const socialNames = Object.getOwnPropertyNames(socialLinks);
-
-        for (const name of socialNames) {
-            document.querySelector(`img[data-social="${name}"]`).addEventListener('click', () => {
-                window.location.href = socialLinks[name];
-            });
-        }
-    }
-
     function onload () {
         const sunsetElement = document.querySelector('img#sunset');
         const sunriseElement = document.querySelector('img#sunrise');
         const socialElements = Array.from(document.querySelectorAll('img.social'));
-
-        linkifySocialImages();
 
         sunsetElement.addEventListener('click', () => {
             document.body.classList.add('night');
